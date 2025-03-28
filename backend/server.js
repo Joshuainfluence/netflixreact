3cf3e495cf9ae23a9dd8fd8f04f97c747cb963b4
unknown <joshuajulius2030@gmail.com>
2025-03-28 11:07:57 +0100

    created envars file for constant variables

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 // const express = require('express'); common js
 import express from 'express'; //esmodule, to be able to use, set type: module in package.json file
 import authRoutes from './routes/auth.route.js'
+import dotenv from 'dotenv'
+import { ENV_VARS } from './config/enVars.js';
+
+dotenv.config();
+
 
 const app = express();
+const PORT = ENV_VARS.PORT
 // the reason why the verson (v1) for api, is that at some point api will be updated, but the project will still need the first api
 app.use("/api/v1/auth", authRoutes)
+
+
 // to be able to run this file, under at package.json file, at scripts, add dev: node
-app.listen(5000, ()=> {
-    console.log("Server is started on https://localhost:5000")
+app.listen(PORT, ()=> {
+    console.log("Server is started on https://localhost:" + PORT)
 })
