1df20ee9291bfe24fe0e5ed3d95c5c8487cda537
unknown <joshuajulius2030@gmail.com>
2025-03-28 09:51:37 +0100

    modified package.json file, and created the server.js file

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+// const express = require('express'); common js
+import express from 'express'; //esmodule, to be able to use, set type: module in package.json file
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Server is ready");
+})
+// to be able to run this file, under at package.json file, at scripts, add dev: node
+app.listen(5000, ()=> {
+    console.log("Server is started on https://localhost:5000")
+})
