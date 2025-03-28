f30c341b224a0c459d2d9f131047ef67eba34570
unknown <joshuajulius2030@gmail.com>
2025-03-28 12:14:20 +0100

    signup function working perfectly

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -22,7 +22,7 @@ export async function signup(req, res) {
             return res.status(400).json({success: false, message: "Username must be between 3 and 20 characters!"});
         }
 
-        const existingUserByEmail = await User.fihdOne({email: email})
+        const existingUserByEmail = await User.findOne({email: email})
         if(existingUserByEmail){
             return res.status(400).json({success: false, message: "Email already exists!"});
         }
@@ -34,7 +34,7 @@ export async function signup(req, res) {
 
         const PROFILE_PICS = ["/avatar1.png", "avatar2.png", "avatar3.png"];
         const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)];
-        const hashedPassword = await bcrypt.hash(password, 10);
+        // const hashedPassword = await bcrypt.hash(password, 10);
 
         const newUser = new User(
             {
@@ -48,6 +48,8 @@ export async function signup(req, res) {
         )
 
         await newUser.save();
+        
+        res.status(200).json({success: true, message: "User created successfully!", newUser});   
     } catch (error) {
         console.log("Error in signup controller", error.message);
         res.status(500).json({success: false, message: "Internal server error!"});
