7c07dd859e8c98343262a10cb29bd163957a10c7
unknown <joshuajulius2030@gmail.com>
2025-03-28 14:41:35 +0100

    get movie trailer controller and routes api working

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -10,3 +10,17 @@ export async function getTrendingMovie(req, res){
         res.status(500).json({success: false, message: "Internal server error"});
     }
 }
+
+export async function getMovieTrailers(req, res) {
+    const {id} = req.params;
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`);
+        res.status(200).json({success: true, trailers: data.results})
+    } catch (error) {
+        if (error.message.includes("404")) {
+            return res.status(400).send(null)
+        }
+
+        res.status(500).json({success: false, message: "Internal server error"});
+    }
+}
--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getTrendingMovie } from '../controllers/movie.controller.js';
+import { getMovieTrailers, getTrendingMovie } from '../controllers/movie.controller.js';
 
 const router = express.Router();
 
 // getting only one trendng movie, when we refresh it will update to another one
 router.get("/trending", getTrendingMovie) ;
+
+router.get("/:id/trailers", getMovieTrailers);
 
 export default router;  
