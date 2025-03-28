db0245e57cde009af11d1debfd9a8255bfb0c17d
unknown <joshuajulius2030@gmail.com>
2025-03-28 17:02:50 +0100

    modified error in search person

--- backend/controllers/search.controller.js
+++ backend/controllers/search.controller.js
@@ -2,12 +2,11 @@ import { User } from "../model/user.model.js";
 import { fetchFromTMDB } from "../services/tmdb.service.js";
 
 export async function searchPerson(req, res) {
-    // https://api.themoviedb.org/3/search/person?include_adult=false&language=en-US&page=1
     const { query } = req.params;
 
     try {
-        const response = fetchFromTMDB(`https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`);
-        if (response.length === 0) {
+        const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`);
+        if (response.results.length === 0) {
             return res.status(400).send(null);
         }
 
@@ -34,7 +33,7 @@ export async function searchMovie(req, res) {
 
     try {
         const response = fetchFromTMDB(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`);
-        if (response.length === 0) {
+        if (response.status.length === 0) {
             return res.status(400).send(null);
         }
 
@@ -63,7 +62,7 @@ export async function searchTv(req, res) {
 
     try {
         const response = fetchFromTMDB(`https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=false&language=en-US&page=1`);
-        if (response.length === 0) {
+        if (response.status.length === 0) {
             return res.status(400).send(null);
         }
 
@@ -87,3 +86,16 @@ export async function searchTv(req, res) {
     }
 }
 
+
+export async function getSearchHistory(req, res) {
+    try {
+        res.status(200).json({ success: true, content: req.user.searchHistory })
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Internal server error" })
+    }
+}
+
+export async function removeItemFromSearchHistory(req, res) {
+    const { id } = req.params;
+
+}
