e46e423d2906716df3546301247d89ddf89ae714
unknown <joshuajulius2030@gmail.com>
2025-03-28 10:12:56 +0100

    created auth routes and added the api endopint in server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
 // const express = require('express'); common js
 import express from 'express'; //esmodule, to be able to use, set type: module in package.json file
+import authRoutes from './routes/auth.route.js'
 
 const app = express();
-
-app.get("/", (req, res) => {
-    res.send("Server is ready");
-})
+// the reason why the verson (v1) for api, is that at some point api will be updated, but the project will still need the first api
+app.use("/api/v1/auth", authRoutes)
 // to be able to run this file, under at package.json file, at scripts, add dev: node
 app.listen(5000, ()=> {
     console.log("Server is started on https://localhost:5000")
