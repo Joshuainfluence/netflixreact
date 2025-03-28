e446e7d9436865fb60839145ee8a28271f9583e9
unknown <joshuajulius2030@gmail.com>
2025-03-28 14:51:31 +0100

    get movie details controller and route api working

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getMovieTrailers, getTrendingMovie } from '../controllers/movie.controller.js';
+import { getMovieDetails, getMovieTrailers, getTrendingMovie } from '../controllers/movie.controller.js';
 
 const router = express.Router();
 
@@ -7,5 +7,7 @@ const router = express.Router();
 router.get("/trending", getTrendingMovie) ;
 
 router.get("/:id/trailers", getMovieTrailers);
+
+router.get("/:id/details", getMovieDetails);
 
 export default router;  
