0767e52694d248239996a506eac48bab03c6a340
unknown <joshuajulius2030@gmail.com>
2025-03-29 10:03:46 +0100

    modified frontend routes and created components folder, adding Footer to it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,11 @@ import LoginPage from "./pages/LoginPage.jsx";
 
 import { Routes, Route } from "react-router-dom";
 import SignUpPage from "./pages/SignUpPage.jsx";
+import Footer from "./components/Footer.jsx";
 
 export default function App() {
   return (
+    <>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
@@ -13,5 +15,7 @@ export default function App() {
 
 
     </Routes>
+    <Footer/>
+    </>
   )
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+<footer className='py-6'></footer>
+  )
+}
+
+export default Footer
