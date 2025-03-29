ba1f28564d35ac83077ed7648686e6dbb28d2646
unknown <joshuajulius2030@gmail.com>
2025-03-29 09:56:28 +0100

    authscreen fouth section

--- frontend/src/pages/home/AuthScreen.jsx
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -144,6 +144,34 @@ const AuthScreen = () => {
             </div>
 
             {/* seperator */}
+
+            <div
+                className='h-2 w-full bg-[#232323]'
+                aria-hidden='true'
+            />
+
+            {/* fourth section */}
+            <div className='py-10 bg-black text-white'>
+                <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col-reverse px-4 md:px-2">
+                    {/* left side */}
+                    <div className="flex-1 relative">
+                        <div className="relative">
+                            <img src="/kids.png" alt="Tv image" className='mt-4' />
+                            <div className="absolute top-0 left-0 w-full h-full bg-black/50 rounded-lg"></div>
+                        </div>
+                    </div>
+
+                    {/* right side */}
+                    <div className="flex-1 text-center md:text-left">
+                        <h2 className="text-4xl md:text-5xl font-extrabold mb-4">
+                            Create profiles for kids.
+                        </h2>
+                        <p className="text-lg md:text-xl">
+                            Send kids on adventures with their favorite characters in a space made just for them—free with your membership.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
