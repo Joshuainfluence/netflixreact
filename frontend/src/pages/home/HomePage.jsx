2b28340aad56e04425a104bde3926dee5766b2ab
unknown <joshuajulius2030@gmail.com>
2025-04-01 12:39:30 +0100

    added loader

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -7,10 +7,10 @@ import { useAuthStore } from '../../store/authUser.js';
 const HomePage = () => {
    const {user} = useAuthStore()
   return (
-     <div>
+     <>
       {/* this the homepage, will display homescreen if user is authenticated else it will display authscreen where the user will have to input email address and then signin/signup */}
         {user ? <HomeScreen/> : <AuthScreen/>}
-     </div>
+     </>
   )
 }
 
