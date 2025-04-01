e62b3d1a9002c6bbf5d13ecbe3e5ebfd1ba7f92a
unknown <joshuajulius2030@gmail.com>
2025-04-01 12:09:17 +0100

    always redirect users to home page if authenticated and there's a signup/login attempt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import HomePage from "./pages/home/HomePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import Footer from "./components/Footer.jsx";
 import { Toaster } from "react-hot-toast";
@@ -19,8 +19,11 @@ export default function App() {
     <>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
+      {/* if  user is visiting the login page while they're authenicated, redirect them back to the home page */}
+
+      <Route path="/login" element={!user ? <LoginPage /> : <Navigate to= {"/"}/>} />
+      {/* if  user is visiting the signup page while they're authenicated, redirect them back to the home page */}
+      <Route path="/signup" element={!user ? <SignUpPage /> : <Navigate to={"/"}/>} />
 
 
     </Routes>
