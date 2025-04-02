f16a2e99bef3b86e93641a2e8373186158b67bac
unknown <joshuajulius2030@gmail.com>
2025-04-02 19:32:45 +0100

    updated movie slider

--- frontend/src/components/MovieSlider.jsx
+++ frontend/src/components/MovieSlider.jsx
@@ -5,66 +5,67 @@ import { Link } from 'react-router-dom';
 import { SMALL_IMG_BASE_URL } from '../utils/constants';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 
-const MovieSlider = ({category}) => {
-    const {contentType} = useContentStore();
+const MovieSlider = ({ category }) => {
+    const { contentType } = useContentStore();
     const formattedCategory = category.charAt(0).toUpperCase() + category.replaceAll("_", " ").slice(1);
-    const formattedContentType = contentType.charAt(0).toUpperCase() + contentType.slice(1); 
+    const formattedContentType = contentType.charAt(0).toUpperCase() + contentType.slice(1);
     const [showArrows, setShowArrows] = useState(false)
     const [content, setContent] = useState([]);
 
     const sliderRef = useRef(null)
 
-    useEffect(()=>{
+    useEffect(() => {
         const getContent = async () => {
             const res = await axios.get(`/api/v1/${contentType}/${category}`)
             setContent(res.data.content)
         }
-         getContent()
+        getContent()
     }, [contentType, category])
 
-    const scrollLeft = ()=> {
+    const scrollLeft = () => {
         if (sliderRef.current) {
-            sliderRef.current.scrollBy({left: -sliderRef.current.offsetWidth, behavior: 'smooth'})
+            sliderRef.current.scrollBy({ left: -sliderRef.current.offsetWidth, behavior: 'smooth' })
         }
     }
-    const scrollRight = ()=> {
+    const scrollRight = () => {
         if (sliderRef.current) {
-            sliderRef.current.scrollBy({left: +sliderRef.current.offsetWidth, behavior: 'smooth'})
+            sliderRef.current.scrollBy({ left: +sliderRef.current.offsetWidth, behavior: 'smooth' })
         }
-            
+
     }
 
-   
-  return (
-    <div className='text-white bg-black relative px-5 md:px-20' onMouseEnter={()=> setShowArrows(true)} onMouseLeave={()=> setShowArrows(false)}>
-        <h2 className='mb-4 text-2xl font-bold'>
-            {formattedCategory} {formattedContentType}
-        </h2>
 
-        <div className="flex space-x-4 overflow-x-scroll" ref={sliderRef}>
-            {content.map((item)=> (
-                <Link to={`/watch/${item.id}`} className='min-w-[250px] relative group' key={item.id}>
-                    <div className="rounded-lg overflow-hidden">
-                        <img src={SMALL_IMG_BASE_URL+item.backdrop_path} alt="Movie image"  className='transition-transform duration-300 ease-in-out group-hover:scale-125'/>
-                    </div>
-                    <p className="mt-2 text-center">
-                        {item.title ||item.name }
-                    </p>
-                </Link>
-            ))}
+    return (
+        <div className='text-white bg-black relative px-5 md:px-20' onMouseEnter={() => setShowArrows(true)} onMouseLeave={() => setShowArrows(false)}>
+            <h2 className='mb-4 text-2xl font-bold'>
+                {formattedCategory} {formattedContentType}
+            </h2>
+
+            {/* scrollbar-hide class is coming from the installed tailwind scrollbar hide package */}
+            <div className="flex space-x-4 overflow-x-scroll scrollbar-hide" ref={sliderRef}>
+                {content.map((item) => (
+                    <Link to={`/watch/${item.id}`} className='min-w-[250px] relative group' key={item.id}>
+                        <div className="rounded-lg overflow-hidden">
+                            <img src={SMALL_IMG_BASE_URL + item.backdrop_path} alt="Movie image" className='transition-transform duration-300 ease-in-out group-hover:scale-125' />
+                        </div>
+                        <p className="mt-2 text-center">
+                            {item.title || item.name}
+                        </p>
+                    </Link>
+                ))}
+            </div>
+            {showArrows && (
+                <>
+                    <button className='absolute top-1/2 -translate-y-1/2 left-5 md:left-24 flex items-center justify-center size-12 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75 text-white z-10' onClick={scrollLeft}>
+                        <ChevronLeft size={24} />
+                    </button>
+                    <button className='absolute top-1/2 -translate-y-1/2 right-5 md:right-24 flex items-center justify-center size-12 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75 text-white z-10' onClick={scrollRight}>
+                        <ChevronRight size={24} />
+                    </button>
+                </>
+            )}
         </div>
-        {showArrows && (
-            <>
-            <button className='absolute top-1/2 -translate-y-1/2 left-5 md:left-24 flex items-center justify-center size-12 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75 text-white z-10' onClick={scrollLeft}>
-            <ChevronLeft size={24} />
-            </button>
-            <button className='absolute top-1/2 -translate-y-1/2 right-5 md:right-24 flex items-center justify-center size-12 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75 text-white z-10' onClick={scrollRight}>
-            <ChevronRight size={24} />
-            </button>
-            </>
-        )}
-    </div>
-  )
+    )
 }
 
 export default MovieSlider
